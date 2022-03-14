87ebb1498507041c334e738c16710d59a80de1c3
qudwn1114 <qudwn1114@gmail.com>
2022-03-14 15:27:15 +0900

    feat: add robots.txt

--- gamdong/urls.py
+++ gamdong/urls.py
@@ -1,8 +1,9 @@
 from django.urls.conf import path
+from django.views.generic import TemplateView
 from gamdong.views import HomeView, CarDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('vehicle-detail/<int:pk>', CarDetailView.as_view(), name='vehicle-detail'),
-
+    path('robots.txt',  TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ]
