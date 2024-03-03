3236772cc3b225e29d310a33e5bac9c82ff3c288
qudwn1114 <qudwn1114@gmail.com>
2024-03-03 16:36:43 +0900

    feat: contact email

--- gamdong/urls.py
+++ gamdong/urls.py
@@ -1,10 +1,9 @@
 from django.urls.conf import path
-from django.views.generic import TemplateView
-from gamdong.views import HomeView, CarDetailView
+from gamdong.views import HomeView, CarDetailView, contact
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('vehicle-detail/<int:pk>', CarDetailView.as_view(), name='vehicle-detail'),
-    path('robots.txt',  TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+    path('contact', contact)
 ]
 
--- gamdong/views.py
+++ gamdong/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, View
+from django.views.generic import View
+from django.views.decorators.http import require_http_methods
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.conf import settings
 from gamdong.models import ElectionVehicle, ElectionVehicleDetail, ElectionVehicleGallery, ElectionVehicleGalleryDetail
 # Create your views here.
 
@@ -31,3 +35,35 @@ class CarDetailView(View):
             context['gallery_detail'] = gallery_detail
 
         return render(request, 'car_detail.html', context)
+
+
+@require_http_methods(["POST"])
+def contact(request: HttpRequest):
+    '''
+        메일 전송
+    '''
+    name = request.POST['name']
+    email = request.POST['email']
+    phone = request.POST['phone']
+    message = request.POST['message']
+    to_email = 'qudwn1114@gmail.com'
+
+    try:
+        message = render_to_string('contact_email.html', {
+            'name' : name,
+            'email' : email,
+            'phone' : phone,
+            'message' : message
+        })
+
+        mail_title = f"[Gamdong Motors] Contact 메일 ({name})"
+        sendEmail = EmailMessage(mail_title, message, settings.EMAIL_HOST_USER, to=[to_email])
+        sendEmail.send()
+    except:
+        return JsonResponse({
+            'message' : '전송 에러...'
+        }, status = 400)
+
+    return JsonResponse({
+        'message' : '이메일이 전송되었습니다.'
+    }, status = 200)
